8cd2858a55930fe7b083b173057572a15a64eb29
Alexander <iguminov05@yandex.ru>
2023-02-08 20:01:56 +0300

    добавлена задержка на инзмение input, добавлен ответ при отсутсвии фильма в базе, а также мелкий рефакторинг

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { Space, Input, Menu, Layout, Pagination, Divider, Spin, Alert, message } from 'antd';
 import { Offline, Online } from 'react-detect-offline';
+import debounce from 'lodash.debounce';
 
 import { MovieItem } from './components/item';
 const { Header, Content, Footer } = Layout;
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [value, setValue] = useState('');
+  const [text, setText] = useState('');
   const [searchData, setSearchData] = useState({});
   const [genresList, setGengesList] = useState({});
   const [page, setPage] = useState(1);
@@ -16,13 +17,39 @@ function App() {
   const [error, setError] = useState(null);
   const [messageApi, contextHolder] = message.useMessage();
 
-  const _key = 'b86a8d724a602ddbef697c551c95e01d';
-  const url = `https://api.themoviedb.org/3/search/movie?api_key=${_key}&query=${value || 'return'}&page=${page}`;
-  const urlGenres = `https://api.themoviedb.org/3/genre/movie/list?api_key=${_key}`;
-
-  const getData = async () => {
+  const getData = async (value) => {
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=b86a8d724a602ddbef697c551c95e01d&query=${
+      value || 'return'
+    }&page=${page}`;
     setLoading(true);
-    await fetch(urlGenres)
+    setError(null);
+    await fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Шеф, все пропало');
+        }
+      })
+      .then((res) => {
+        console.log(res);
+        setSearchData(res);
+        setMovies(res.results);
+        if (!res.results.length) {
+          throw new Error('Ничего не найдено');
+        }
+      })
+      .catch((e) => {
+        setError(e);
+        errorMessage(e);
+      });
+    setLoading(false);
+  };
+
+  const getGenres = () => {
+    const urlGenres = 'https://api.themoviedb.org/3/genre/movie/list?api_key=b86a8d724a602ddbef697c551c95e01d';
+    setLoading(true);
+    fetch(urlGenres)
       .then((response) => {
         if (response.ok) {
           return response.json();
@@ -34,49 +61,31 @@ function App() {
       .catch((e) => {
         errorMessage(e);
       });
-    await fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error('Шеф, все пропало');
-        }
-      })
-      .then((res) => {
-        setSearchData(res);
-        setMovies(res.results);
-      })
-      .catch((e) => {
-        setError(e);
-        errorMessage(e);
-      });
-    setLoading(false);
   };
+
   useEffect(() => {
+    setLoading(true);
     getData();
-    setValue('');
   }, [page]);
+
+  useEffect(() => {
+    setLoading(true);
+    getGenres();
+  }, []);
 
   const errorMessage = (error) => {
     messageApi.open({
       content: <Alert message={error.message} type="error" showIcon banner />,
-      duration: 5,
+      duration: 3,
     });
   };
 
   const searchInput = (event) => {
-    setValue(event.target.value);
+    setText(event.target.value);
+    debouceSearchInput(event.target.value);
   };
+  const debouceSearchInput = useMemo(() => debounce(getData, 500), []);
 
-  const getSearch = (event) => {
-    setLoading(true);
-    if (event.key === 'Enter') {
-      if (value.length) {
-        getData();
-        setLoading(false);
-      }
-    }
-  };
   const ratedMovies = () => {
     const id = JSON.parse(localStorage.getItem('id'));
     const newData = fiteredRatedMovies(id, movies);
@@ -118,24 +127,20 @@ function App() {
           </Header>
           <Content style={{ width: '1010px' }}>
             <Input
-              style={{ width: '900px', margin: 30 }}
+              style={{ width: '930px', margin: '30px 40px' }}
               placeholder="Type to search..."
-              value={value}
+              value={text}
               onChange={searchInput}
-              onKeyUp={getSearch}
             />
             <Space
               style={{
                 display: 'flex',
-                // width: '100vw',
-
-                // height: 570,
                 flexWrap: 'wrap',
                 backgroundColor: '#f5f5f5',
                 justifyContent: 'center',
               }}
             >
-              {error && <Alert message={error.message} type="error" showIcon width="100%" />}
+              {error && <Alert message={error.message} type="error" showIcon style={{ width: '900px' }} />}
               {loading && <Spin size="large" spinning={loading} tip="Loading..." />}
               {!loading &&
                 fiteredMovies &&
